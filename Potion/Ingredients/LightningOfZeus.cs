0c758cbafaed7e4d0de6af90bb6874981aee9e5f
Nikita Romanenko <nikiromanen@gmail.com>
2023-01-15 21:28:20 +0200

    Created derivative class LightningOfZeus form Ingredient

---
+++ Potion/Ingredients/LightningOfZeus.cs
@@ -0,0 +1,18 @@
+﻿namespace Potion.Ingredients
+{
+    internal class LightningOfZeus : Ingredient
+    {
+        string[] compatibilityElementsWithLightning = { "Water", "Fire" };
+
+        public LightningOfZeus() 
+        {
+            element = "Lightning";
+            numberOfEmptyFlask++;
+
+            foreach (var item in compatibilityElementsWithLightning)
+            {
+                compatibilityElement.Add(item);
+            }
+        }
+    }
+}
