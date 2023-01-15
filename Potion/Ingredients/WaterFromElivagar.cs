7f86078879f679e35429aae106b9ecd1394258e3
Nikita Romanenko <nikiromanen@gmail.com>
2023-01-15 21:34:47 +0200

    Created derivative class LightningOfZeus form Ingredient

---
+++ Potion/Ingredients/WaterFromElivagar.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Linq;
+
+namespace Potion.Ingredients
+{
+    internal class WaterFromElivagar : Ingredient
+    {
+        string[] compatibilityElementsWithWater = { "Water", "Soil", "Lightning" };
+
+        public WaterFromElivagar()
+        {
+            element = "Water";
+            numberOfEmptyFlask++;
+
+            foreach (var item in compatibilityElementsWithWater)
+            {
+                compatibilityElement.Add(item);
+            }
+        }
+    }
+}
