5451a45a1b16da8a38b6db7fedbdf4bad4638f8e
Nikita Romanenko <nikiromanen@gmail.com>
2023-01-16 20:06:44 +0200

    Created verification in method CreatedPotion

--- Potion/Ingredients/Ingredient.cs
+++ Potion/Ingredients/Ingredient.cs
@@ -1,10 +1,59 @@
 ﻿namespace Potion.Ingredients
 {
-    abstract internal class Ingredient
+    internal class Ingredient
     {
-        protected int numberOfEmptyFlask { get; set; }
-        protected string element { get; set; }
+        Gamester gamester = new();
 
-        protected List<string> compatibilityElement = new();
+        protected int numberOfIngtrdients { get; set; }
+        protected string name { get; set; }
+        
+        protected List<string> elements = new();
+        protected List<string> notCompatibilityElement = new();
+
+        public string CreatedPotion(params Ingredient[] ingredients)
+        {
+            List<string> notCompatibilityAllElements = new();
+            List<string> elementsAllIngredients = new();
+
+            
+            foreach (var item in ingredients)
+            {
+
+                for (int i = 0; i < item.notCompatibilityElement.Count; i++)
+                {
+                    notCompatibilityAllElements.Add(item.notCompatibilityElement[i]);
+
+                }
+
+                for (int i = 0; i < item.elements.Count; i++)
+                {
+                    elementsAllIngredients.Add(item.elements[i]);
+                }
+
+                if (item.numberOfIngtrdients <= 0)
+                {
+                    return $"You don't have {item.name}";
+                }
+                else
+                {
+                    item.numberOfIngtrdients--;
+                }
+            }
+
+            foreach (var item in notCompatibilityAllElements)
+            {
+                if (elementsAllIngredients.Contains(item))
+                {
+                    return "Fail!";
+                }
+            }
+
+            if (gamester.numberOfEmptyFlask <= 0)
+            {
+                return "You don't have empty Flask";
+            }
+
+            return "SUCCESS!!!";
+        }
     }
 }
