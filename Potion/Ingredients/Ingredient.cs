f4f3e02b1a796ee440bb54359e84bf8ee6350029
Nikita Romanenko <nikiromanen@gmail.com>
2023-01-18 21:34:40 +0200

    Made an element of randomness when checking non-removable elements

--- Potion/Ingredients/Ingredient.cs
+++ Potion/Ingredients/Ingredient.cs
@@ -2,31 +2,38 @@
 {
     internal class Ingredient
     {
+        Random Randomrandom = new();
         Gamester gamester = new();
 
         protected int numberOfIngtrdients { get; set; }
         protected string name { get; set; }
-        
-        protected List<string> ingredientElements = new();
+
+        protected List<string> mainElementsIngredient = new();
+        protected List<string> secondaryElementsIngredient = new();
         protected List<string> elementsIncompatibleWithTheIngredient = new();
 
         public string CreatePotion(params Ingredient[] ingredients)
         {
+            List<string> mainElementsAllIngredient = new();
+            List<string> secondaryElementsAllIngredients = new();
             List<string> notCompatibilityAllElements = new();
-            List<string> elementsAllIngredients = new();
             
             
             foreach (var item in ingredients)
             {
+                for (int i = 0; i < item.mainElementsIngredient.Count; i++)
+                {
+                    mainElementsAllIngredient.Add(item.mainElementsIngredient[i]);
+                }
+
+                for (int i = 0; i < item.secondaryElementsIngredient.Count; i++)
+                {
+                    secondaryElementsAllIngredients.Add(item.secondaryElementsIngredient[i]);
+                }
                 
                 for (int i = 0; i < item.elementsIncompatibleWithTheIngredient.Count; i++)
                 {
                     notCompatibilityAllElements.Add(item.elementsIncompatibleWithTheIngredient[i]);
-                }
-
-                for (int i = 0; i < item.ingredientElements.Count; i++)
-                {
-                    elementsAllIngredients.Add(item.ingredientElements[i]);
                 }
 
                 if (item.numberOfIngtrdients <= 0)
@@ -41,9 +48,16 @@
 
             foreach (var item in notCompatibilityAllElements)
             {
-                if (elementsAllIngredients.Contains(item))
+                if (mainElementsAllIngredient.Contains(item))
                 {
                     return "Fail!";
+                }
+                else if (secondaryElementsAllIngredients.Contains(item))
+                {
+                    if (Randomrandom.Next(2) == 0)
+                    {
+                        return "Fail";
+                    }
                 }
             }
 
