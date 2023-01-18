af26160b196864763ab3fe3cdb7fb702a40d5635
Nikita Romanenko <nikiromanen@gmail.com>
2023-01-18 21:33:18 +0200

    Made a category of main and minor elements

--- Potion/Ingredients/LightningOfZeus.cs
+++ Potion/Ingredients/LightningOfZeus.cs
@@ -2,22 +2,28 @@
 {
     internal class LightningOfZeus : Ingredient
     {
+        private string[] mainElementsIngredient = { "Lightning" };
+        private string[] secondaryElementsIngredient = { "Shine", "Sound", "Energy" };
         private string[] elementsIncompatibleWithTheIngredient = { "Blood" };
-        private string[] ingredientElements = { "Lightning", "Shine", "Sound", "Energy" };
 
         internal LightningOfZeus() 
         {
             name = "Lightning of Zeus";
             numberOfIngtrdients++;
 
+            foreach (var item in mainElementsIngredient)
+            {
+                base.mainElementsIngredient.Add(item);
+            }
+
+            foreach (var item in secondaryElementsIngredient)
+            {
+                base.secondaryElementsIngredient.Add(item);
+            }
+
             foreach (var item in elementsIncompatibleWithTheIngredient)
             {
                 base.elementsIncompatibleWithTheIngredient.Add(item);
-            }
-
-            foreach (var item in ingredientElements)
-            {
-                base.ingredientElements.Add(item);
             }
         }
     }
--- Potion/Ingredients/WaterFromElivagar.cs
+++ Potion/Ingredients/WaterFromElivagar.cs
@@ -2,22 +2,28 @@
 {
     internal class WaterFromElivagar : Ingredient
     {
+        private string[] mainElementsIngredient = { "Water" };
+        private string[] secondaryElementsIngredient = { "Life", "Time" };
         private string[] elementsIncompatibleWithTheIngredient = { "Air" };
-        private string[] ingredientElements = { "Water", "Life", "Time" };
 
         internal WaterFromElivagar()
         {
             name = "Water from Elivagar";
             numberOfIngtrdients++;
+            
+            foreach (var item in mainElementsIngredient)
+            {
+                base.mainElementsIngredient.Add(item);
+            }
+
+            foreach (var item in secondaryElementsIngredient)
+            {
+                base.secondaryElementsIngredient.Add(item);
+            }
 
             foreach (var item in elementsIncompatibleWithTheIngredient)
             {
                 base.elementsIncompatibleWithTheIngredient.Add(item);
-            }
-
-            foreach (var item in ingredientElements)
-            {
-                base.ingredientElements.Add(item);
             }
         }
     }
